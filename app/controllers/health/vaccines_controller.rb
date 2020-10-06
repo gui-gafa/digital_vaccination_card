0327ee3bf6e4877b7050a0d34a7f617fa017e4c9
Guilherme Pelusi <guilherme.pelusi@gmail.com>
2020-10-06 15:23:02 -0300

    validação de doses

--- app/controllers/health/vaccines_controller.rb
+++ app/controllers/health/vaccines_controller.rb
@@ -7,4 +7,21 @@ class Health::VaccinesController < ApplicationController
       @vaccines = Vaccine.where(user_id: params[:user_id]).joins(:vaccine_type).order('vaccine_types.name')
     end
   end
+
+  def edit
+    @vaccine = Vaccine.find(params[:id])
+    @doses = @vaccine.doses.order(:date).reject { |dose| dose.user.present? }
+  end
+
+  def update
+    @doses = Dose.find(doses_params[:id])
+    @doses.each do |dose|
+      dose.update(user: current_user)
+    end
+    redirect_to health_vaccines_path(user_id: @doses.first.vaccine.user.id)
+  end
+
+  def doses_params
+    params.require(:vaccine).require(:dose_ids).permit(id: [])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
   namespace :health do
     resources :users, only: [:index]
-    resources :vaccines, only: [:index, :edit]
+    resources :vaccines, only: [:index, :edit, :update]
   end
   devise_for :users
   root to: 'pages#home'
